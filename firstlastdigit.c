bf7e34901e80a03d7891315e12d597258da52b31
Bhavyasunkavall <101538469+Bhavyasunkavalli@users.noreply.github.com>
2022-05-31 15:45:32 +0000

    output added

--- firstlastdigit.c
+++ firstlastdigit.c
@@ -11,10 +11,13 @@ r=n%10;
 n=n/10;
 while(n>0)
 {
-r=n%10;
+r1=n%10;
 n=n/10;
 }
 printf("last digit is %d",r);
 printf("first digit is %d",r1);
 return 0;
 }
+output:
+enter any number:46
+last digit is 6first digit is 4
